41716ac6778f2c8d9545d0c1424b699ba4b0753b
Shirley Moore <svmoore@utep.edu>
2020-09-08 08:30:17 -0600

    routine to output binary string

---
+++ C_Examples/Chapter2/itoa.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <string.h>
+
+void reverse(char *s)
+{
+   int length, c;
+   char *begin, *end, temp;
+ 
+   length = strlen(s);
+   begin  = s;
+   end    = s;
+ 
+   for (c = 0; c < length - 1; c++)
+      end++;
+ 
+   for (c = 0; c < length/2; c++)
+   {        
+      temp   = *end;
+      *end   = *begin;
+      *begin = temp;
+ 
+      begin++;
+      end--;
+   }
+}
+
+// Implementation of itoa() 
+char* itoa(int num, char* str, int base) 
+{ 
+    int i = 0; 
+    int isNegative = 0; 
+  
+    /* Handle 0 explicitely, otherwise empty string is printed for 0 */
+    if (num == 0) 
+    { 
+        str[i++] = '0'; 
+        str[i] = '\0'; 
+        return str; 
+    } 
+  
+    // In standard itoa(), negative numbers are handled only with  
+    // base 10. Otherwise numbers are considered unsigned. 
+    if (num < 0 && base == 10) 
+    { 
+        isNegative = 1; 
+        num = -num; 
+    } 
+  
+    // Process individual digits 
+    while (num != 0) 
+    { 
+        int rem = num % base; 
+        str[i++] = (rem > 9)? (rem-10) + 'a' : rem + '0'; 
+        num = num/base; 
+    } 
+  
+    // If number is negative, append '-' 
+    if (isNegative) 
+        str[i++] = '-'; 
+  
+    str[i] = '\0'; // Append string terminator 
+  
+    // Reverse the string 
+    reverse(str); 
+  
+    return str; 
+} 
+
+/*
+// Driver program to test implementation of itoa() 
+int main() 
+{ 
+    char str[100]; 
+    printf("Base 10:%s\n", itoa(1567, str, 10)); 
+    printf("Base 10:%s\n", itoa(-1567, str, 10)); 
+    printf("Base 2:%s\n", itoa(1567, str, 2)); 
+    printf("Base 2:%s\n", itoa(1567, str, 8)); 
+    printf("Base 16:%s\n", itoa(1567, str, 16)); 
+    return 0; 
+} 
+*/
