0e68900595c6500e92affbbe9a7365d222f6ef3b
Miguel Contreras <miguelacontreras08@gmail.com>
2021-09-06 16:47:10 -0600

    Modified farenheit2celsius-for.c

--- C_Examples/Chapter1/fahrenheit2celsius-for.c
+++ C_Examples/Chapter1/fahrenheit2celsius-for.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
-
-main()
+int main()
 {
+	float convert(float x){
+		return (5.0/9.0)*(x-32);
+	}
 	  int fahr;
 
 	  for (fahr = 0; fahr <= 300; fahr = fahr + 20)
 		    printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+	  
+	  printf("%6.1f",convert(20));
 }
