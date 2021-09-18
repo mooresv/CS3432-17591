f71bd089f2ce8dcd2f763fed8c9873ff981f762e
Shirley Moore <svmoore@utep.edu>
2021-09-18 16:32:32 -0600

    removed unnecessary variables, added calls to invert and rightrot (uncomment to test)

--- C_Examples/Chapter2/testbits.c
+++ C_Examples/Chapter2/testbits.c
@@ -5,6 +5,7 @@
 extern unsigned int invert(unsigned int, int, int);
 extern unsigned int setbits(unsigned int, int, int, unsigned int);
 extern unsigned int getbits(unsigned int, int, int);
+extern unsigned int rightrot(unsigned int, int);
 
 void showbits( unsigned int x )
 {
@@ -21,13 +22,10 @@ int main()
         unsigned int x, y, a;
         int p;
         int n;
-        char buffer[100];
-        char outbuf[100];
-        int width = 32;
-        int len;
 
         printf("This program gets n bits from position p in number x\n");
-        printf("This program inverts the n bits that begin at position p in x\n");
+//        printf("This program inverts the n bits that begin at position p in x\n");
+//        printf("This program rotates x right by n bit positions\n");
         printf("This program then sets the n bits that begin at position p in x to the rightmost n bits of y\n");
         printf("Enter value of x in hexadecimal: ");
         scanf("%x", &x);
@@ -44,12 +42,17 @@ int main()
         printf("Enter value of n in decimal: ");
         scanf("%d", &n);
 	a = getbits(x, p, n);
-	printf("getbits(x%08x, %d, %d): x%08x\n", x, p, n, a);
+	printf("\ngetbits(x%08x, %d, %d): x%08x\n", x, p, n, a);
         printf("binary: ");
         showbits(a);
         printf("\n");
 /*	a = invert(x, p, n);
 	printf("invert(x%08x, %d, %d): x%08x\n", x, p, n, a);
+        printf("binary: ");
+        showbits(a);
+        printf("\n");
+	a = rightrot(x, n);
+	printf("rightrot(x%08x, %d): x%08x\n", x, n, a);
         printf("binary: ");
         showbits(a);
         printf("\n");
